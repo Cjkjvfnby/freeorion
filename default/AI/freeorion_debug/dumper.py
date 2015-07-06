cecf32f1e3463ac0c64dbedb479f8537052ada25
Cjkjvfnby <Cjkjvfnby@gmail.com>
2015-07-07 01:13:41 +0300

    Refactor dumpers.

--- default/AI/freeorion_debug/dumper.py
+++ default/AI/freeorion_debug/dumper.py
@@ -5,99 +5,18 @@ from EnumsAI import AIFleetMissionType, TargetType
 
 sys.path.append('c:/Python27/Lib/site-packages/')
 
-from AIFleetMission import AIFleetMission
 import freeOrionAIInterface as fo
 import FreeOrionAI as foAI
 
 uid_time_format = '%y-%m-%d_%H-%M-%S%f'
 
 
-def dump_data():
-    empire = fo.getEmpire()
-    dumper = Dumper('%s_%s_%s' % (empire.empireID, foAI.foAIstate.uid, empire.name.replace(' ', '_')))
-    turn_uid = foAI.foAIstate.get_current_turn_uid()
-    data = {
-
-        'turn_uid': turn_uid,
-        'parent_uid': foAI.foAIstate.get_prev_turn_uid(),
-        'turn': fo.currentTurn(),
-    }
-
-    # dump functions should define next keys:
-    #     - id:  unique id for item used to get diff for universe objects it can be objectID
-    dump_planets(dumper, **data)
-    dump_fleets(dumper, **data)
-    dump_orders(dumper, **data)
-
-
-def dump_planets(dumper, **kwargs):
-    universe = fo.getUniverse()
-    planets = []
-    headers = ['id', 'pid', 'name', 'size', 'focus', 'sid', 'owned', 'owner', 'visibility', 'species']
-
-    for pid in universe.planetIDs:
-        planet = universe.getPlanet(pid)
-        data = {
-                'id': pid,
-                'pid': pid,
-                'name': planet.name,
-                'size': planet.size.name,
-                'focus': planet.focus,
-                'sid': planet.systemID,
-                'owned': not planet.unowned,
-                'owner': planet.owner if planet.owner != -1 else None,
-                'species': planet.speciesName,
-                'visibility': str(universe.getVisibility(pid, fo.getEmpire().empireID))
-                }
-        assert set(headers) == set(data.keys()), 'keys is not mutch headers: headers: %s, keys: %s' (headers, data.keys())
-        planets.append(data)
-    planets.sort(key=lambda x: x['sid'])
-    kwargs['headers'] = headers
-    dumper.dump('planets', kwargs, planets)
-
-
-def dump_fleets(dumper, **kwargs):
-    universe = fo.getUniverse()
-    headers = ['id', 'fid', 'name', 'sid', 'owner', 'visibility', 'ships', 'target']
-    fleets = []
-    destroyed = set(universe.destroyedObjectIDs(fo.getEmpire().empireID))
-    for fid in universe.fleetIDs:
-        if fid in destroyed:
-            continue
-        fleet = universe.getFleet(fid)
-        mission = foAI.foAIstate.get_fleet_mission(fid)
-        data = {
-                'id': fid,
-                'fid': fid,
-                'name': fleet.name,
-                'sid': fleet.systemID,
-                'owner': fleet.owner,
-                'visibility': str(universe.getVisibility(fid, fo.getEmpire().empireID)),
-                'ships': list(fleet.shipIDs),
-                }
-
-        if mission:
-            assert isinstance(mission, AIFleetMission)
-            for k, v in mission._mission_types.items():
-                if v:
-                    for item in v:
-                        obj = item.target_obj
-                        if obj:
-                            name = obj.name
-                        else:
-                            name = "???"
-                        data['target'] = [AIFleetMissionType.name(k), item.target_id, TargetType().name(item.target_type), name]
-                        break
-
-        assert set(headers).issuperset(set(data.keys())), 'keys is not mutch headers: headers: %s, keys: %s' % (headers, data.keys())
-        fleets.append(data)
-        # data.update(kwargs)
-    kwargs['headers'] = headers
-    dumper.dump('fleets', kwargs, fleets)
-
-
 class Dumper(object):
+    HEADERS = None
+    NAME = None
+
     def __init__(self, uid):
+        assert 'id' in self.HEADERS, '"id" header is required'
         current_folder = os.path.dirname(__file__)
         dums_path = os.path.join(current_folder, 'dumps')
         if not os.path.exists(dums_path):
@@ -107,31 +26,129 @@ class Dumper(object):
             os.mkdir(game_folder)
         self.game_folder = game_folder
 
-    def dump(self, section, common_info, item_list):
+    def get_items(self):
+        raise NotImplementedError()
+
+    def construct_item(self, item):
+        raise NotImplementedError()
+
+    def sort(self, collection):
+        return collection.sort(key=lambda x: x['id'])
+
+    def _dump(self, section, common_info, item_list):
         """
         Add serialized values to file.
-        :param section: file name
-        :param values: list of dicts with data.
         """
         file_path = os.path.join(self.game_folder, section)
         with open(file_path, 'a') as f:
             f.write(json.dumps([common_info, item_list]))
             f.write('\n')
 
+    def dump(self, **kwargs):
+        self.turn = kwargs['turn']
+        result = []
+        for item in self.get_items():
+            data = self.construct_item(item)
+            assert set(self.HEADERS).issuperset(data), 'Keys does not present in headers: %s' % set(data).difference(
+                self.HEADERS)
+            result.append(data)
+        self.sort(result)
+        kwargs['headers'] = self.HEADERS
+        self._dump(self.NAME, kwargs, result)
 
-def dump_orders(dumper, **kwargs):
-    from freeorion_debug.extend_free_orion_AI_interface import turn_dumps
-    headers = ['id', 'name', 'args']
-    turn = fo.currentTurn()
-    turn_orders = turn_dumps.get(turn, [])
-    orders = []
-    for i, order in enumerate(turn_orders):
+
+class DumpPlanets(Dumper):
+    HEADERS = ['id', 'pid', 'name', 'size', 'focus', 'sid', 'owned', 'owner', 'visibility', 'species']
+    NAME = 'planets'
+
+    def sort(self, collection):
+        collection.sort(key=lambda x: x['pid'])
+        collection.sort(key=lambda x: x['sid'])
+
+    def get_items(self):
+        return fo.getUniverse().planetIDs
+
+    def construct_item(self, pid):
+        universe = fo.getUniverse()
+        planet = universe.getPlanet(pid)
         data = {
-            'id': '%s-%s' % (turn, i),
-            'name': order[0],
-            'args': order[1]
+            'id': pid,
+            'pid': pid,
+            'name': planet.name,
+            'size': planet.size.name,
+            'focus': planet.focus,
+            'sid': planet.systemID,
+            'owned': not planet.unowned,
+            'owner': planet.owner if planet.owner != -1 else None,
+            'species': planet.speciesName,
+            'visibility': str(universe.getVisibility(pid, fo.getEmpire().empireID))
         }
-        assert set(headers).issuperset(set(data.keys())), 'keys is not mutch headers: headers: %s, keys: %s' % (headers, data.keys())
-        orders.append(data)
-    kwargs['headers'] = headers
-    dumper.dump('orders', kwargs, orders)
+        return data
+
+
+class DumpFleet(Dumper):
+    HEADERS = headers = ['id', 'fid', 'name', 'sid', 'owner', 'visibility', 'ships', 'target']
+    NAME = 'fleets'
+
+    def get_items(self):
+        universe = fo.getUniverse()
+        return set(universe.fleetIDs) - set(universe.destroyedObjectIDs(fo.getEmpire().empireID))
+
+    def construct_item(self, fid):
+        universe = fo.getUniverse()
+        fleet = universe.getFleet(fid)
+        mission = foAI.foAIstate.get_fleet_mission(fid)
+        data = {
+            'id': fid,
+            'fid': fid,
+            'name': fleet.name,
+            'sid': fleet.systemID,
+            'owner': fleet.owner,
+            'visibility': str(universe.getVisibility(fid, fo.getEmpire().empireID)),
+            'ships': list(fleet.shipIDs),
+        }
+        if mission:
+            for k, v in mission._mission_types.items():
+                if v:
+                    for item in v:
+                        obj = item.target_obj
+                        if obj:
+                            name = obj.name
+                        else:
+                            name = "???"
+                        data['target'] = [AIFleetMissionType.name(k), item.target_id,
+                                          TargetType().name(item.target_type), name]
+        return data
+
+
+class DumpOrders(Dumper):
+    HEADERS = ['id', 'name', 'args']
+    NAME = 'orders'
+
+    def get_items(self):
+        from freeorion_debug.extend_free_orion_AI_interface import turn_dumps
+
+        turn = fo.currentTurn()
+        turn_orders = turn_dumps.get(turn, [])
+        return enumerate(turn_orders)
+
+    def construct_item(self, order_pairs):
+        i, (name, args) = order_pairs
+        return {
+            'id': '%s-%s' % (self.turn, i),
+            'name': name,
+            'args': args
+        }
+
+
+def dump_data():
+    empire = fo.getEmpire()
+    uniq_key = '%s_%s_%s' % (empire.empireID, foAI.foAIstate.uid, empire.name.replace(' ', '_'))
+    turn_uid = foAI.foAIstate.get_current_turn_uid()
+    data = {
+        'turn_uid': turn_uid,
+        'parent_uid': foAI.foAIstate.get_prev_turn_uid(),
+        'turn': fo.currentTurn(),
+    }
+    for cls in (DumpPlanets, DumpFleet, DumpOrders):
+        cls(uniq_key).dump(**data)
